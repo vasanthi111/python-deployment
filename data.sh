c11e36b18362295ff98fa5bed4a7c910cc718527
vasanthi111 <125528976+vasanthi111@users.noreply.github.com>
2024-12-24 17:37:58 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,9 +3,9 @@ sudo apt update -y
 sudo apt install git -y
 sudo apt install python3-pip -y
 sudo apt install pip3 -y
-mkdir /bhargavi
+mkdir /vasu
 cd /home/ubuntu/
-git clone https://github.com/bhargavibairagoni/USA-Housing.git
+git clone https://github.com/vasanthi111/USA-Housing.git
 cd /home/ubuntu/USA-Housing
 pip3 install -r requirements.txt
 screen -m -d python3 ./app.py
