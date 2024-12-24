30e21c0ea65f2705b84274ed214b326bcb8bbf3b
vasanthi111 <125528976+vasanthi111@users.noreply.github.com>
2024-12-24 18:08:15 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "public_subnet-1" {
 ami="ami-005fc0f236362e99f"
 instance_type="t2.micro"
 count=1
-key_name="new-key"
+key_name="ec1"
 vpc_security_group_ids=["${aws_security_group.demosg.id}"]
 subnet_id="${aws_subnet.public_subnet-1.id}"
 associate_public_ip_address=true
